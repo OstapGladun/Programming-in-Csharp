8ec893e179d1478cec98339b19c2a8e5753b3bd0
OstapGladun <48804608+OstapGladun@users.noreply.github.com>
2025-03-27 00:10:09 +0200

    Lab02 added

---
+++ Lab02/MainWindowViewModel.cs
@@ -0,0 +1,128 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab02
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private DateTime _birthDate = DateTime.Now;
+        private string _resultText;
+        private bool _isCalculating = false;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+                CheckIsProceedPossible();
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+                CheckIsProceedPossible();
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+                CheckIsProceedPossible();
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set
+            {
+                _birthDate = value;
+                OnPropertyChanged(nameof(BirthDate));
+                CheckIsProceedPossible();
+            }
+        }
+
+        public string ResultText
+        {
+            get { return _resultText; }
+            set
+            {
+                _resultText = value;
+                OnPropertyChanged(nameof(ResultText));
+            }
+        }
+
+        public bool IsProceedPossible { get; set; }
+        public DelegateCommand ProceedCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ProceedCommand = new DelegateCommand(async () => await ProceedAsync());
+        }
+
+        private void CheckIsProceedPossible()
+        {
+            IsProceedPossible = !string.IsNullOrEmpty(FirstName) && !string.IsNullOrEmpty(LastName) && !string.IsNullOrEmpty(Email) && BirthDate != null/* && !_isCalculating*/;
+            OnPropertyChanged(nameof(IsProceedPossible));
+        }
+
+        private async Task ProceedAsync()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - BirthDate.Year;
+            if (today.DayOfYear < BirthDate.DayOfYear)
+                age--;
+            if (age < 0)
+            {
+                MessageBox.Show($"Помилка: вказана дата ({BirthDate.Year}.{BirthDate.Month}.{BirthDate.Day}) ще не настала", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else if (age > 135)
+            {
+                MessageBox.Show("Помилка: вам більше 135 років", "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            _isCalculating = true;
+            CheckIsProceedPossible();
+
+            Person newPerson = new Person(FirstName, LastName, Email, BirthDate);
+            await newPerson.CalculatePropertiesAsync();
+
+            _isCalculating = false;
+            CheckIsProceedPossible();
+
+            ResultText = newPerson.AllPropertiesString();
+            if (newPerson.IsBirthday)
+                MessageBox.Show("З Днем народження!", "Привітання", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public event PropertyChangedEventHandler  PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Lab02/Person.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public DateTime BirthDate { get; }
+
+        public bool IsAdult { get; private set; }
+        public string SunSign { get; private set; }
+        public string ChineseSign { get; private set; }
+        public bool IsBirthday { get; private set; }
+
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName, email, DateTime.MinValue) { }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+            : this(firstName, lastName, "", birthDate) { }
+
+        public async Task CalculatePropertiesAsync()
+        {
+            await Task.Run(() =>
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - BirthDate.Year;
+                if (today.DayOfYear < BirthDate.DayOfYear)
+                    age--;
+                IsAdult = (age >= 18);
+                SunSign = GetWesternZodiac(BirthDate);
+                ChineseSign = GetChineseZodiac(BirthDate.Year);
+                IsBirthday = (today.Month == BirthDate.Month && today.Day == BirthDate.Day);
+            });
+        }
+
+        public string AllPropertiesString()
+        {
+            return $"Name: {FirstName}\nSurname: {LastName}\nEmail: {Email}\nDate of birth: {BirthDate.Day}.{BirthDate.Month}.{BirthDate.Year}\nYou are{(IsAdult ? " " : " not ")}an adult \nWestern zodiac sign: {SunSign}\nChinese zodiac sign: {ChineseSign}\nToday is{(IsBirthday?" ":" not ")}your birthday";
+        }
+
+        private string GetWesternZodiac(DateTime birthDate)
+        {
+            int day = birthDate.Day;
+            int month = birthDate.Month;
+            switch (month)
+            {
+                case 1:
+                    return (day <= 20) ? "Козеріг" : "Водолій";
+                case 2:
+                    return (day <= 19) ? "Водолій" : "Риби";
+                case 3:
+                    return (day <= 20) ? "Риби" : "Овен";
+                case 4:
+                    return (day <= 20) ? "Овен" : "Телець";
+                case 5:
+                    return (day <= 21) ? "Телець" : "Близнюки";
+                case 6:
+                    return (day <= 21) ? "Близнюки" : "Рак";
+                case 7:
+                    return (day <= 22) ? "Рак" : "Лев";
+                case 8:
+                    return (day <= 22) ? "Лев" : "Діва";
+                case 9:
+                    return (day <= 23) ? "Діва" : "Терези";
+                case 10:
+                    return (day <= 23) ? "Терези" : "Скорпіон";
+                case 11:
+                    return (day <= 22) ? "Скорпіон" : "Стрілець";
+                case 12:
+                    return (day <= 21) ? "Стрілець" : "Козеріг";
+                default:
+                    return "Невідомий знак";
+            };
+        }
+
+        private string GetChineseZodiac(int year)
+        {
+            string[] chineseZodiacs =
+            {
+                "Мавпа", "Півень", "Собака", "Свиня", "Щур", "Бик",
+                "Тигр", "Кролик", "Дракон", "Змія", "Кінь", "Коза"
+            };
+            return chineseZodiacs[year % 12];
+        }
+
+    }
+}
